fe3637d3bc97191a185829a07f4ba398f07f896e
Karan Pahawa <kpahawa@gmail.com>
2015-12-16 23:26:31 +0900

    trying new ui changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,19 +2,11 @@ require 'securerandom'
 class WelcomeController < ApplicationController
   def home
     @num = Contact.all.length
-    @articles = Article.all
-    @arr = Array.new
-    x = 0
-    for i in 0...@articles.size
-      @arr.push(@articles[i ])
-      x+=1
-      if x == 5
-        break
-      end
-    end
+    @arr = Article.last
 
     @projects = Project.all
     @proj = Array.new
+
     x = 0
     for j in 0...@projects.size
       @proj.push(@projects[j])
@@ -27,6 +19,8 @@ class WelcomeController < ApplicationController
 
     @pictures = Picture.all
     @pics = Array.new
+    @pic = Picture.last
+
     x = 0
     for j in 0...@pictures.size
       @pics.push(@pictures[j])
@@ -40,22 +34,4 @@ class WelcomeController < ApplicationController
   def contact
    @word = generateNew
   end
-
-  private
-    def generateNew
-      r = Random.new
-      start = r.rand(3...5)
-      sec = SecureRandom.hex(5)
-      endIndex = start + 4
-      @word = sec[start..endIndex]
-      return @word
-    end
-
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
-
-    def contact_params
-      params.require(:contact).permit(:first, :last, :email, :subscribed, :message)
-    end
 end
