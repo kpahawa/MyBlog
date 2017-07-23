fedf51cf20608940091f439702c022348cdacfd7
kpahawa <k661993P>
2017-07-23 00:40:09 -0400

    fixed icons loading problem

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,35 +1,7 @@
 require 'securerandom'
 class WelcomeController < ApplicationController
   def home
-    @num = Contact.all.length
-    @arr = Article.last
 
-    @num_arr = (Article.all).size
-    @projects = Project.all
-    @proj = Array.new
-
-    x = 0
-    for j in 0...@projects.size
-      @proj.push(@projects[j])
-      x+=1
-      if x == 5
-        break
-      end
-    end
-
-
-    @pictures = Picture.all
-    @pics = Array.new
-    @pic = Picture.last
-
-    x = 0
-    for j in 0...@pictures.size
-      @pics.push(@pictures[j])
-      x+=1
-      if x == 6
-        break
-      end
-    end
   end
 
   def contact
